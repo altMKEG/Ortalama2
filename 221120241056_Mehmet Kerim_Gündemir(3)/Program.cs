3a48db13b718d3eae7e9e80b4b497c98014d5fa5
Mehmet Kerim Gündemir <gundemirkerim9@gmail.com>
2024-11-21 02:02:04 +0300

    Proje dosyası ekle.

---
+++ 221120241056_Mehmet Kerim_Gündemir(3)/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _221120241056_Mehmet_Kerim_Gündemir_3_
+{
+    internal class Program
+    {
+        static public void Öğrenci()
+        {
+            string adi = "Mehmet Kerim";
+            string soyadi = "Gündemir";
+
+            Console.WriteLine(adi + soyadi);
+            
+        }
+        static public void Hesapla()
+        {
+            int not1 = 50;
+            int not2 = 76;
+
+            Console.WriteLine(not1 + not2 / 2);
+
+
+        } 
+        static void Main(string[] args) 
+        {
+            Öğrenci();
+            Hesapla();      
+        }
+    }
+
+}
---
+++ 221120241056_Mehmet Kerim_Gündemir(3)/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("221120241056_Mehmet Kerim_Gündemir(3)")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("221120241056_Mehmet Kerim_Gündemir(3)")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("be765c6b-ffde-4c0e-a4f8-adf43401a074")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
